767ad0a95b4f55990d65abab69e559d6c986556a
dansut24 <Danieljamessutton18@outlook.com>
2026-05-30 22:21:42 +0100

    Update inventory.go

--- agent/internal/inventory/inventory.go
+++ agent/internal/inventory/inventory.go
@@ -1,21 +1,35 @@
 package inventory
 
 import (
+	"os"
 	"runtime"
 )
 
 type Inventory struct {
-	CPU string
-	RAM int
-	Disk int
-	GPU string
+	CPU              string
+	RAMGB            int
+	DiskGB           int
+	GPU              string
+	LoggedInUser     string
+	BitLockerEnabled bool
+	TPMVersion       string
 }
 
 func Collect() Inventory {
+
+	user := os.Getenv("USER")
+
+	if user == "" {
+		user = os.Getenv("USERNAME")
+	}
+
 	return Inventory{
-		CPU: runtime.GOARCH,
-		RAM: 0,
-		Disk: 0,
-		GPU: "Unknown",
+		CPU:              runtime.GOARCH,
+		RAMGB:            0,
+		DiskGB:           0,
+		GPU:              "Unknown",
+		LoggedInUser:     user,
+		BitLockerEnabled: false,
+		TPMVersion:       "Unknown",
 	}
 }
