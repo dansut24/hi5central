4b464eb1c0f44e938da3a3d97723c0b20e74a082
dansut24 <Danieljamessutton18@outlook.com>
2026-05-30 22:01:29 +0100

    Create config.go

---
+++ agent/internal/config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	APIURL     string `json:"apiUrl"`
+	TenantSlug string `json:"tenantSlug"`
+}
+
+func Load(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
