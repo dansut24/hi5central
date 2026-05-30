15ce401259ef6ac3650b48e0299beaacf5c180e1
dansut24 <Danieljamessutton18@outlook.com>
2026-05-30 13:04:40 +0100

    Create tokens.ts

---
+++ packages/auth/src/tokens.ts
@@ -0,0 +1,12 @@
+import crypto from "node:crypto";
+
+export function generateToken(size = 32) {
+  return crypto.randomBytes(size).toString("hex");
+}
+
+export function hashToken(token: string) {
+  return crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+}
